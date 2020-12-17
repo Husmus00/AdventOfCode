f3673270ff92c7c4308c68df2e421177e7676bc1
Hussam Alhassan <husmus7g@protonmail.com>
2020-12-18 01:29:20 +0300

    Moved puzzle folders to 2020 directory

--- Puzzle_2-1/target/debug/Puzzle_2-1.d
+++
@@ -1,1 +0,0 @@
-D:\05-\ Projects\AdventOfCode\Puzzle_2-1\target\debug\puzzle_2-1.exe: D:\05-\ Projects\AdventOfCode\Puzzle_2-1\src\main.rs
--- Puzzle_2-1/target/debug/deps/Puzzle_2_1.d
+++
@@ -1,5 +0,0 @@
-D:\05- Projects\AdventOfCode\Puzzle_2-1\target\debug\deps\puzzle_2_1.exe: src\main.rs
-
-D:\05- Projects\AdventOfCode\Puzzle_2-1\target\debug\deps\puzzle_2_1.d: src\main.rs
-
-src\main.rs:
